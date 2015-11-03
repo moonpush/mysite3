b954bd9b77ac59108895b6e536f03f6ce8649248
bit-user <bit-user@222.106.22.80>
2015-11-03 15:07:32 +0900

    board sql 추가

---
+++ docs/board-test.sql
@@ -0,0 +1,29 @@
+--insert(새글)
+insert
+  into board
+values ( board_no_seq.nextval, 
+		 '첫글입니다.', 
+		 '안녕하세요', 
+		 4,
+		 0, 
+		 nvl( ( select max( group_no ) from board ), 0) + 1,
+		 1,
+		 1,
+		 SYSDATE );
+
+
+
+
+
+-------------------------------------------------
+-- 연습
+-------------------------------------------------
+
+-- 문장열 더하기 연산자 ||
+select title || '+++++++++++++++' || content from board;
+
+-- nvl 함수
+select nvl( null, 0 ) from dual;
+
+delete from board;
+commit;
